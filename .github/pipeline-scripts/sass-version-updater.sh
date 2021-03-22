5db08fdd48e33c4219f33882b8ad7236500853f5
[GHA] Hegistvan <github-actions@hegistvan.com>
2021-03-22 15:29:34 -0400

    Test-change-detection

--- .github/pipeline-scripts/sass-version-updater.sh
+++ .github/pipeline-scripts/sass-version-updater.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-{ set -euo pipefail; } 2>/dev/null
+{ set -euxo pipefail; } 2>/dev/null
 
 VERSION=$(curl  -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/sass/dart-sass/releases/latest | jq -r ".tag_name" | sed -s 's/^v//')
 
 sed -i '1c ARG UPSTREAM_VERSION='"${VERSION}" Dockerfile
-if $(! git diff-index --quiet HEAD -- Dockerfile); then
+if [ -n "$(git --no-pager diff HEAD -- Dockerfile)" ]; then
     git version
     git config user.name '[GHA] Hegistvan'
     git config user.email 'github-actions@hegistvan.com'
@@ -12,5 +12,5 @@ if $(! git diff-index --quiet HEAD -- Dockerfile); then
     git add Dockerfile
     git commit -m "Dart SASS version update to '${VERSION}'"
     git tag -fa "v${VERSION}" $(git rev-parse --short HEAD)
-    git push origin --follow-tags
+    echo "# git push origin --follow-tags"
 fi
